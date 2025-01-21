bade80680550f37ee8568cb1fd006b84e63033c6
Votre nom <votre-email@example.com>
2025-01-21 20:29:14 +0100

    modifs Challenges + Partage 3

--- app/api/challenges/share/route.js
+++ app/api/challenges/share/route.js
@@ -1,10 +1,12 @@
 // app/api/challenges/share/route.js
 
+
+
 import multer from 'multer';
 import path from 'path';
 import { NextResponse } from 'next/server';
 
-// Configurer multer pour l'upload de fichiers
+// Configuration de multer pour l'upload de fichiers localement
 const upload = multer({
     storage: multer.diskStorage({
         destination: './public/uploads/', 
@@ -14,34 +16,33 @@ const upload = multer({
     }),
 });
 
-// Désactiver le bodyParser directement dans la fonction de gestion de requête
 export const runtime = 'edge';
 
+// Gestion de l'upload des fichiers via POST
 export async function POST(req) {
     return new Promise((resolve, reject) => {
-        console.log('Début de la gestion de l\'upload');  // Log ajouté
+        console.log('Début de la gestion de l\'upload'); 
 
         upload.single('photo')(req, {}, (err) => {
             if (err) {
-                console.error('Erreur d\'upload:', err);  // Log détaillé de l'erreur
+                console.error('Erreur d\'upload:', err); 
                 reject(
                     NextResponse.json({ message: "Erreur lors de l'upload de l'image." }, { status: 500 })
                 );
-                return; // Arrêter l'exécution si une erreur survient
+                return;
             }
 
-            // Vérifier si req.file existe
             if (!req.file) {
-                console.error('Aucun fichier trouvé dans req.file');  
+                console.error('Aucun fichier trouvé dans req.file');
                 reject(
                     NextResponse.json({ message: "Aucun fichier n'a été téléchargé." }, { status: 400 })
                 );
                 return;
             }
 
-            const photoUrl = `/uploads/${req.file.filename}`; 
-            console.log('Upload réussi, URL de l\'image:', photoUrl);  // Log de l'URL de l'image
-
+            // Générer l'URL de la photo uploadée
+            const photoUrl = `/uploads/${req.file.filename}`;
+            console.log('Upload réussi, URL de l\'image:', photoUrl);
 
             resolve(
                 NextResponse.json({ photoUrl }, { status: 200 })
